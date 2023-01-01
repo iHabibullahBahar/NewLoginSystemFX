544fc50c049c003c91d6ce13bb791fbe190985a4
Habib <Habib@Habib.Dlink>
2023-01-01 08:08:25 +0600

    commit

---
+++ src/loginsystem/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginsystem;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Habib
+ */
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+    
+}
